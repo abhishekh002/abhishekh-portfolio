2ac7c86d3afd353439ab242f597337682ca88d9c
abhishekh002 <abhisekh31tiwari@gmail.com>
2026-07-10 23:46:34 +0530

    feat: add GitHub Pages deployment workflow and fix vite base path

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/abhishekh-portfolio/',
   plugins: [react()],
+  base: '/abhishekh-portfolio/',
   server: {
     allowedHosts: true
   }
